78c4f2dcdc0344bd47d0b6a6579d45abbf03717f
Akın Kebabcı <kebabci12@gmail.com>
2022-10-04 00:23:48 +0300

    JavaPatikalari

---
+++ Java101/DesenMetot/src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+        static void f(int x){
+            int output = x -5;
+            if(output>=-4) {
+
+                System.out.print(output + " ");
+                f(output);
+
+                int temp = output + 5;
+                if(temp<=x){
+                    System.out.print(temp + " ");
+                }
+            }
+        }
+
+        public static void main(String[] args) {
+            Scanner s = new Scanner(System.in);
+
+            System.out.print("Sayı Giriniz : ");
+            int input = s.nextInt();
+
+            System.out.print(input + " ");
+            f(input);
+
+
+    }
+}
